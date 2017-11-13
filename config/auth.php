b0c126565e89cd377497c6f8701d3d10614042b9
luis enrique <luisesh2@hotmail.com>
2017-11-12 20:57:38 -0600

    subir

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => Tomato\User::class,
+            'model' => Tomato\Models\User::class,
         ],
 
         // 'users' => [
